a33607ac423af5a4eeffd3116d0acea0fe7f13b2
Zhonya2 <geka04082007@gmail.com>
2025-11-27 22:42:02 +0200

    Delete KanbanWebAPI.Domain directory

--- KanbanWebAPI.Domain/Entities/Team.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KanbanWebAPI.Domain.Entities;
-
-public class Team
-{
-    public Guid TeamId { get; init; }
-    public string TeamName { get; set; } = null!;
-    public IReadOnlyCollection<User> Users { get; set; } = new List<User>();
-    public IReadOnlyCollection<Board> Boards { get; set; } = new List<Board>();
-}
