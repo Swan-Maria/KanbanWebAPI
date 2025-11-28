9a125a894fd51ebdee6705d2ab3ffa889662a5fc
Zhonya2 <geka04082007@gmail.com>
2025-11-28 15:25:38 +0200

    Update Program.cs

--- KanbanWebAPI.WebApi/Program.cs
+++ KanbanWebAPI.WebApi/Program.cs
@@ -1,26 +1,13 @@
-using KanbanWebAPI.Application.Mapping;
+using KanbanWebAPI.Application;
 using KanbanWebAPI.Application.Interfaces.Services;
-using KanbanWebAPI.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Connecting AutoMapper from your Application layer
-builder.Services.AddAutoMapper(typeof(KanbanProfile).Assembly);
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITeamService, TeamService>();
-builder.Services.AddScoped<IBoardService, BoardService>();
-builder.Services.AddScoped<IColumnService, ColumnService>();
-builder.Services.AddScoped<ITaskService, TaskService>();
-builder.Services.AddScoped<ITaskAssignmentService, TaskAssignmentService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
-builder.Services.AddScoped<ITagService, TagService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ITaskAuditService, TaskAuditService>();
+// Connect Application via extension-method
+builder.Services.AddApplicationLayer();
 
 var app = builder.Build();
 
