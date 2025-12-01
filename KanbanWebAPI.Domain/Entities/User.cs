5547a4fbc571b6647df0cfb90e07557a429fabfb
IvanNklv <nikolaevivan229@gmail.com>
2025-12-02 00:21:30 +0200

    Update User.cs

--- KanbanWebAPI.Domain/Entities/User.cs
+++ KanbanWebAPI.Domain/Entities/User.cs
@@ -1,11 +1,18 @@
-﻿namespace KanbanWebAPI.Domain.Entities;
+namespace KanbanWebAPI.Domain.Entities;
 
 public class User
 {
     public Guid UserId { get; set; }
-    public string GoogleId { get; init; } = null!;
+
+    public string GoogleId { get; set; } = null!;
+
     public string Username { get; set; } = null!;
     public string Email { get; set; } = null!;
+
+    public string? PasswordHash { get; set; }
+    public string? PasswordSalt { get; set; }
+
     public IReadOnlyCollection<Team> Teams { get; set; } = new List<Team>();
+    public IReadOnlyCollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
     public IReadOnlyCollection<TaskAudit> TaskAudits { get; set; } = new List<TaskAudit>();
 }
