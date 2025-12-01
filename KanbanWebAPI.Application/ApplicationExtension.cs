152ff6bc7b3a3dc0ef7593359e4262ae1981ab03
Zhonya2 <geka04082007@gmail.com>
2025-12-02 00:36:05 +0200

    Update ApplicationExtension.cs

--- KanbanWebAPI.Application/ApplicationExtension.cs
+++ KanbanWebAPI.Application/ApplicationExtension.cs
@@ -1,22 +1,25 @@
+using AutoMapper;
+using KanbanWebAPI.Application.Mapping;
 using KanbanWebAPI.Application.Interfaces.Services;
 using KanbanWebAPI.Application.Services;
-
 using Microsoft.Extensions.DependencyInjection;
 
-namespace KanbanWebAPI.Application;
-
-public static class ApplicationExtension
+namespace KanbanWebAPI.Application
 {
-    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    public static class ApplicationExtension
     {
-        // Реєстрація сервісів Application рівня
-        services.AddScoped<IUserService, UserService>();
-        services.AddScoped<ITeamService, TeamService>();
-        services.AddScoped<IBoardService, BoardService>();
-        services.AddScoped<IColumnService, ColumnService>();
-        services.AddScoped<ITaskService, TaskService>();
-        services.AddScoped<ITaskAuditService, TaskAuditService>();
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(KanbanProfile).Assembly);
 
-        return services;
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITeamService, TeamService>();
+            services.AddScoped<IBoardService, BoardService>();
+            services.AddScoped<IColumnService, ColumnService>();
+            services.AddScoped<ITaskService, TaskService>();
+            services.AddScoped<ITaskAuditService, TaskAuditService>();
+
+            return services;
+        }
     }
 }
